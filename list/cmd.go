a54cc0017a88861ea71e2a63b3f0fbe6d32b4b05
Michael Bridgen <mikeb@squaremobius.net>
2016-06-23 19:40:31 +0100

    Show git commit messages alongside images
    (These are not in any order, yet)

--- list/cmd.go
+++ list/cmd.go
@@ -17,6 +17,7 @@ import (
 	"github.com/CenturyLinkLabs/docker-reg-client/registry"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	git "gopkg.in/libgit2/git2go.v24"
 )
 
 const DockerConfigFile = "~/.docker/config.json"
@@ -24,6 +25,7 @@ const DockerHub = "https://index.docker.io/v1/"
 
 type listOpts struct {
 	registry        string
+	gitrepo         string
 	useDockerConfig bool
 	username        string
 	password        string
@@ -34,6 +36,17 @@ func (opts *listOpts) run(_ *cobra.Command, args []string) error {
 		return fmt.Errorf("expected argument <image>")
 	}
 	image := args[0]
+
+	var (
+		repo *git.Repository
+		err  error
+	)
+	if opts.gitrepo != "" {
+		repo, err = git.OpenRepository(opts.gitrepo)
+		if err != nil {
+			return err
+		}
+	}
 
 	baseURL, err := url.Parse(opts.registry)
 	if err != nil {
@@ -68,9 +81,43 @@ func (opts *listOpts) run(_ *cobra.Command, args []string) error {
 
 	out := tabwriter.NewWriter(os.Stdout, 0, 4, 1, ' ', 0)
 	for tag, _ := range tags {
-		fmt.Fprintln(out, tag)
+		fmt.Fprint(out, tag)
+		if repo != nil {
+			fmt.Fprint(out, "\t")
+			additional := ""
+			// hard-code tag format for now
+			if strings.HasSuffix(tag, "-WIP") {
+				additional = " (uncommitted changes)"
+				tag = tag[:len(tag)-4]
+			}
+			commit, otherwise := commitFromTag(repo, tag)
+			if otherwise != "" {
+				fmt.Fprintf(out, otherwise)
+			} else {
+				fmt.Fprintf(out, strings.Split(commit.Message(), "\n")[0])
+			}
+			fmt.Fprintf(out, additional)
+		}
+		fmt.Fprint(out, "\n")
 	}
+	out.Flush()
 	return nil
+}
+
+func commitFromTag(repo *git.Repository, tag string) (*git.Commit, string) {
+	bits := strings.Split(tag, "-")
+	if len(bits) != 2 {
+		return nil, "tag does not correspond to a commit"
+	}
+	commitRev, err := repo.RevparseSingle(bits[1])
+	if err != nil {
+		return nil, err.Error()
+	}
+	commit, err := commitRev.AsCommit()
+	if err != nil {
+		return nil, err.Error()
+	}
+	return commit, ""
 }
 
 func AddSubcommandTo(cmd *cobra.Command) {
@@ -81,6 +128,7 @@ func AddSubcommandTo(cmd *cobra.Command) {
 		RunE:  opts.run,
 	}
 	subcmd.Flags().StringVar(&opts.registry, "image-registry", DockerHub, "image registry to query")
+	subcmd.Flags().StringVar(&opts.gitrepo, "repository", "", "path to git repository to cross-reference images to")
 	subcmd.Flags().StringVar(&opts.username, "username", "", "username to use in basic authentication")
 	subcmd.Flags().StringVar(&opts.password, "password", "", "password to use in basic authentication")
 	subcmd.Flags().BoolVar(&opts.useDockerConfig, "use-docker-config", true, fmt.Sprintf("look up the authentication token in %s", DockerConfigFile))
