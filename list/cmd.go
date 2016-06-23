0016edec19130fd4dbb6baf68e7cf926dfb978c5
Michael Bridgen <mikeb@squaremobius.net>
2016-06-23 17:21:50 +0100

    Use entries in Docker config, and account for dockerhub
    The entries in the Docker config are base64 encoded credentials; I had
    guessed they were outright tokens (i.e., something you can just stick
    in a header as-is).
    
    Docker Hub acts as an _index_ rather than a registry, so we have to do
    an extra step to get a token for the actual registry.
    
    Now you can do
    
        flumux list weaveworks/scope
    
    and get an answer, since the registry defaults to Docker Hub and it
    now does the extra token step.

--- list/cmd.go
+++ list/cmd.go
@@ -2,21 +2,31 @@ package list
 
 import (
 	//	"encoding/json"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 	//	"io/ioutil"
 	//	"net/http"
 	"net/url"
 	//"os"
+	"text/tabwriter"
 
 	"github.com/CenturyLinkLabs/docker-reg-client/registry"
-	//	homedir "github.com/mitchellh/go-homedir"
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 )
 
+const DockerConfigFile = "~/.docker/config.json"
+const DockerHub = "https://index.docker.io/v1/"
+
 type listOpts struct {
-	registry string
-	username string
-	password string
+	registry        string
+	useDockerConfig bool
+	username        string
+	password        string
 }
 
 func (opts *listOpts) run(_ *cobra.Command, args []string) error {
@@ -35,17 +45,30 @@ func (opts *listOpts) run(_ *cobra.Command, args []string) error {
 
 	if opts.password != "" || opts.username != "" {
 		auth = registry.BasicAuth{opts.username, opts.password}
+	} else if opts.useDockerConfig {
+		auth = findConfiguredAuth(baseURL)
 	}
 
 	client := registry.NewClient()
 	client.BaseURL = baseURL
 
+	// If it's Docker hub (and possibly others?), we have to go
+	// through this extra step of getting a token
+	if opts.registry == DockerHub {
+		auth, err = client.Hub.GetReadTokenWithAuth(image, auth)
+		if err != nil {
+			return err
+		}
+	}
+
 	tags, err := client.Repository.ListTags(image, auth)
 	if err != nil {
 		return err
 	}
+
+	out := tabwriter.NewWriter(os.Stdout, 0, 4, 1, ' ', 0)
 	for tag, _ := range tags {
-		fmt.Println(tag)
+		fmt.Fprintln(out, tag)
 	}
 	return nil
 }
@@ -57,52 +80,61 @@ func AddSubcommandTo(cmd *cobra.Command) {
 		Short: "list images",
 		RunE:  opts.run,
 	}
-	subcmd.Flags().StringVar(&opts.registry, "image-registry", "https://index.docker.io/v1/", "image registry to query")
+	subcmd.Flags().StringVar(&opts.registry, "image-registry", DockerHub, "image registry to query")
 	subcmd.Flags().StringVar(&opts.username, "username", "", "username to use in basic authentication")
 	subcmd.Flags().StringVar(&opts.password, "password", "", "password to use in basic authentication")
+	subcmd.Flags().BoolVar(&opts.useDockerConfig, "use-docker-config", true, fmt.Sprintf("look up the authentication token in %s", DockerConfigFile))
 
 	cmd.AddCommand(subcmd)
 }
 
 // --- using the Docker config file to get auth
 
-// const DockerConfigFile = "~/.docker/config.json"
+type auth struct {
+	Auth  string `json:"auth"`
+	Email string `json:"email"`
+}
 
-// type token struct {
-// 	Auth  string `json:"auth"`
-// 	Email string `json:"email"`
-// }
+type dockerConfig struct {
+	Auths map[string]auth `json:"auths"`
+}
 
-// type imageTag struct {
-// 	Layer string `json:"layer"`
-// 	Name  string `json:"name"`
-// }
+func findConfiguredAuth(baseURL *url.URL) registry.Authenticator {
+	var result registry.Authenticator = registry.NilAuth{}
+	config, err := loadDockerConfig()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Warning: could not find and read Docker config file")
+		return result
+	}
+	for _, possibleKey := range []string{baseURL.String(), baseURL.Host} {
+		if entry, hasConfig := config.Auths[possibleKey]; hasConfig {
+			asString, err := base64.StdEncoding.DecodeString(entry.Auth)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Warning: could not decode auth entry for %s\n", baseURL)
+				return result
+			}
+			asSlice := strings.SplitN(string(asString), ":", 2)
+			result = &registry.BasicAuth{
+				Username: asSlice[0],
+				Password: asSlice[1],
+			}
+		}
+	}
+	return result
+}
 
-// type dockerConfig struct {
-// 	Auths map[string]token `json:"auths"`
-// }
-
-// func findConfiguredAuth(config *dockerConfig, baseURL *url.URL) *token {
-// 	for _, possibleKey := range []string{baseURL.String(), baseURL.Host} {
-// 		if authEntry, hasConfig := config.Auths[possibleKey]; hasConfig {
-// 			return &authEntry
-// 		}
-// 	}
-// 	return nil
-// }
-
-// func loadDockerConfig() (*dockerConfig, error) {
-// 	path, err := homedir.Expand(DockerConfigFile)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	bytes, err := ioutil.ReadFile(path)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	var config dockerConfig
-// 	if err = json.Unmarshal(bytes, &config); err != nil {
-// 		return nil, err
-// 	}
-// 	return &config, nil
-// }
+func loadDockerConfig() (*dockerConfig, error) {
+	path, err := homedir.Expand(DockerConfigFile)
+	if err != nil {
+		panic(err)
+	}
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var config dockerConfig
+	if err = json.Unmarshal(bytes, &config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
